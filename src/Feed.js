c84d15b90594cd040fb352e89cd1f21a23f7171f
Jimmi Pisalita <mig.jimmi@gmail.com>
2021-12-18 21:41:59 +0100

    First commit. Header + Sidebar. Started Feed

---
+++ src/Feed.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Feed.css';
+
+const Feed = () => {
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Feed
---
+++ src/Header.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import logo from './linkedin.png';
+import avatar from './CatPicture.jpg'
+import HeaderOption from './HeaderOption';
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+function Header() {
+  return (
+    <div className='header'>
+
+      <div className="header__left">
+        <img 
+          src={logo} 
+          alt="" 
+        />
+
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text"/>
+        </div>
+      </div>
+
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title='home'/>
+        <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption avatar={avatar} title='me' />
+
+      </div>
+    </div>
+  )
+}
+
+export default Header
